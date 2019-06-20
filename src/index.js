2a1c3580bd3d95ba997eed88a6dc60775dc4d3dd
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-20 18:03:10 +0300

    Install dependency: bootstrap-material-design

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { render } from 'inferno';
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
+
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 import './index.css';
 
 render(<App />, document.getElementById('root'));
