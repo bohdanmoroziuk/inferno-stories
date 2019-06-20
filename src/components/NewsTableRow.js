197b54ae9096667956bb7e257972466e8f7860f7
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-20 19:58:00 +0300

    Add NewsTableRow component

---
+++ src/components/NewsTableRow.js
@@ -0,0 +1,19 @@
+import Button from './shared/Button';
+
+const NewsTableRow = ({ news, hideNews }) => (
+  <tr class="news-item" key={news.objectID}>
+    <td className="p-3">
+      <a href={news.url}>{news.title}</a>
+    </td>
+    <td className="p-3">{news.author}</td>
+    <td className="p-3">{news.num_comments}</td>
+    <td className="p-3">{news.points}</td>
+    <td className="p-3">
+      <Button className="btn btn-danger btn-sm" onClick={hideNews}>
+        Hide
+      </Button>
+    </td>
+  </tr>
+);
+
+export default NewsTableRow;
