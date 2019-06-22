0bed2525bb88c5bea0d0c36cb2e60e2373b7b930
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-22 10:21:20 +0300

    Change prop names and add an onSubmit handler

--- src/components/forms/Search.js
+++ src/components/forms/Search.js
@@ -1,15 +1,22 @@
-const Search = ({ query, placeholder, handleQueryChange }) => (
-  <form className="form search-form">
-    <div className="form-group">
-      <input
-        className="form-control"
-        type="text"
-        name="query"
-        id="query"
-        value={query}
-        onInput={handleQueryChange}
-        placeholder={placeholder}
-      />
+import Button from '../shared/Button';
+
+const Search = ({ value, onChange, onSubmit, placeholder }) => (
+  <form className="form search-form" {...{ onSubmit }}>
+    <div className="d-flex align-items-center justify-content-between">
+      <div className="form-group w-100 mb-0">
+        <input
+          className="form-control"
+          type="text"
+          name="query"
+          id="query"
+          value={value}
+          onInput={onChange}
+          placeholder={placeholder}
+        />
+      </div>
+      <Button className="btn btn-primary btn-sm ml-2" type="submit">
+        Search
+      </Button>
     </div>
   </form>
 );
