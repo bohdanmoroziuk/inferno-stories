8456e6c07a533e1dce1426e7745e62bd5851f244
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-22 12:37:32 +0300

    Add Loader component

---
+++ src/components/shared/Loader.js
@@ -0,0 +1,12 @@
+const Loader = () => (
+  <div className="d-flex align-items-center justify-content-center px-3">
+    <div class="loader">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+);
+
+export default Loader;
