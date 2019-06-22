f695f8d896f509cca91dfb361e7ecc6baab9b745
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-22 12:16:53 +0300

    Add property type checking for the Header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,13 @@
-const Header = () => (
+import { string } from 'prop-types';
+
+const Header = ({ title }) => (
   <header className="header">
-    <h1 className="title text-center">News</h1>
+    <h1 className="title text-center">{title}</h1>
   </header>
 );
+
+Header.propTypes = {
+  title: string.isRequired
+};
 
 export default Header;
