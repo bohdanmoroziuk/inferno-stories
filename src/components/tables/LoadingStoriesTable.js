eafe96639faacb3050e004207c151dedb89deede
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-23 18:08:05 +0300

    Add LoadingStoriesTable component

---
+++ src/components/tables/LoadingStoriesTable.js
@@ -0,0 +1,6 @@
+import withLoading from '../../hocs/withLoading';
+import StoriesTable from './StoriesTable';
+
+const LoadingStoriesTable = withLoading(StoriesTable);
+
+export default LoadingStoriesTable;
