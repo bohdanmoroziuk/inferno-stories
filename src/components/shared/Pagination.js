bec2f5244ad279b3f3b526920a62a82f174d98e3
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-16 11:16:03 +0300

    Rewrite component using functions

--- src/components/shared/Pagination.js
+++ src/components/shared/Pagination.js
@@ -1,52 +1,40 @@
-import { Component } from 'inferno';
+const Pagination = ({ currentPage, totalPages, changePage }) => {
+  const hasPrev = currentPage > 0;
 
-export default class Pagination extends Component {
-  get hasPrev() {
-    return this.props.currentPage > 0;
-  }
+  const hasNext = currentPage < totalPages;
 
-  get hasNext() {
-    return this.props.currentPage < this.props.totalPages;
-  }
-
-  prevPage = () => {
-    const { currentPage, changePage } = this.props;
-
-    this.hasPrev && changePage(currentPage - 1);
+  const prevPage = () => {
+    hasPrev && changePage(currentPage - 1);
   };
 
-  nextPage = () => {
-    const { currentPage, changePage } = this.props;
-
-    this.hasNext && changePage(currentPage + 1);
+  const nextPage = () => {
+    hasNext && changePage(currentPage + 1);
   };
 
-  render() {
-    const { currentPage, totalPages } = this.props;
-
-    return (
-      <nav aria-label="Page navigation">
-        <div className="d-flex align-items-center justify-content-between">
-          <p>
-            <span className="mb-0 text-muted">Total pages: {totalPages}</span>
-            <span className="mb-0 ml-2 text-muted">
-              Current page: {currentPage + 1}
+  return (
+    <nav aria-label="Page navigation">
+      <div className="d-flex align-items-center justify-content-between">
+        <p>
+          <span className="mb-0 text-muted">Total pages: {totalPages}</span>
+          <span className="mb-0 ml-2 text-muted">
+            Current page: {currentPage + 1}
+          </span>
+        </p>
+        <ul class="pagination justify-content-end">
+          <li class={hasPrev ? 'page-item' : 'page-item disabled'}>
+            <span class="page-link" onClick={prevPage}>
+              &laquo;
             </span>
-          </p>
-          <ul class="pagination justify-content-end">
-            <li class={this.hasPrev ? 'page-item' : 'page-item disabled'}>
-              <span class="page-link" onClick={this.prevPage}>
-                &laquo;
-              </span>
-            </li>
-            <li class={this.hasNext ? 'page-item' : 'page-item disabled'}>
-              <span class="page-link" onClick={this.nextPage}>
-                &raquo;
-              </span>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+          </li>
+          <li class={hasNext ? 'page-item' : 'page-item disabled'}>
+            <span class="page-link" onClick={nextPage}>
+              &raquo;
+            </span>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Pagination;
