e93235229d620f381ac21d4ce381fb80f707ad24
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-16 12:31:47 +0300

    Remove duplicate code

--- src/redux/modules/stories/constants.js
+++ src/redux/modules/stories/constants.js
@@ -1,20 +1,7 @@
 const WIDGET_NAME = 'stories';
 const APP_NAME = process.env.INFERNO_APP_NAME;
 
-const API = {
-  DEFAULT_QUERY: 'redux',
-  DEFAULT_HPP: 5,
-
-  PATH_BASE: 'https://hn.algolia.com/api/v1',
-  PATH_SEARCH: '/search?',
-
-  PARAM_SEARCH: 'query=',
-  PARAM_PAGE: 'page=',
-  PARAM_HPP: 'hitsPerPage='
-};
-
 export default {
   WIDGET_NAME,
   APP_NAME,
-  API,
 };
--- src/redux/modules/stories/index.js
+++ src/redux/modules/stories/index.js
@@ -2,7 +2,6 @@ import reducer from './reducer';
 
 export { default as types } from './types';
 export { default as actions } from './actions';
-export { default as constants } from './constants';
 export { default as selectors } from './selectors';
 export { default as operations } from './operations';
 
